911e13dcc56243c56a7b8306e3204e88f995249b
KenanRico <kenan.hu@mail.utoronto.ca>
2019-09-25 08:26:28 -0400

    add process struct

--- include/process.h
+++ include/process.h
@@ -8,6 +8,21 @@ using ProcFuncPtr = void(Process::*)();
 template <std::size_t N>
 class Process{
 	private:
+		DWORD process_ID;
 		ProcFuncPtr proc_func[N];
 		std::string msg;
+	public:
+		inline const std::string& GetMsg() const { return msg; }
+		void GenerateMemoryProfile();
+		void GenerateCPUProfile();
+		void GenerateOpenFileProfile();
+		void GenerateOpenPortProfile();
+		
+	public:
+		Process(DWORD);
+		~Process();
+	private:
+		Process() = delete;
+		Process(const Process&) = delete;
+		Process& operator=(const Process&) = delete;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char** argv){
 		exit(0);
 	}
 	if(enabled_flags[Flags::HELP]){
-		// ?
+		// print help page
 	}
 
 	/*
